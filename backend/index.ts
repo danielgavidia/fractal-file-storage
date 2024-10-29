d46c7fd8bb75dbb6dda1b08872a0c6404151e249
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-29 11:29:25 -0400

    Setup test route for express server

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes will go here
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world!");
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
