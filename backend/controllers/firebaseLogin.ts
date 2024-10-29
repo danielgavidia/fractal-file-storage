b6587108d0d835d12b203ece3dfc2cc2c49fdd63
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-29 14:25:22 -0400

    Added routes for login and signup

---
+++ backend/controllers/firebaseLogin.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from "express";
+import { getUserLogin } from "../prisma/prismaAuth";
+import type { User } from "../types";
+
+export const firebaseLogin = async (req: Request, res: Response) => {
+  const { firebaseId } = req.body;
+  const user: User = await getUserLogin(firebaseId);
+  console.log(user);
+  res.status(200).json({ user: user });
+};
